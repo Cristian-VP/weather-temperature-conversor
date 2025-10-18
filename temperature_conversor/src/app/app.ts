a5f02c6b05023dc195e2297a73763d336a585693
Cristian Andrés Vacacela Procel <cvacacelaprocel@cifpfbmoll.eu>
2025-10-18 13:12:09 +0000

    Removed: Modules. Added: CSS Variables, basic template to banner

--- temperature_conversor/src/app/app.ts
+++ temperature_conversor/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { WeatherComponent } from './weather.component/weather.component';
+import { MatCard, MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    WeatherComponent,
+    MatCardModule
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
